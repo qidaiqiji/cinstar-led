bb173a2d17da6155a139e160c74dc7479de509da
qidaiqiji <459306493@qq.com>
2024-09-26 18:08:24 +0800

    增加404页面

---
+++ error/404.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include('../inc/rem.php'); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="/css/common-240917.css">
+    <link rel="stylesheet" href="/css/contact-240815.css">
+    <link rel="stylesheet" href="/css/404.css">
+    <title>Ice Hockey Media Cube LED Video Wall Panels | Cinstar LED</title>
+    <meta name="keywords" content="media cube, ice hockey, mediakub, LED video wall panels, LED display screen, sports arena, led system, led technology" />
+    <meta name="description" content="Cinstar P4 ice hockey media cube LED video wall panels shine at Lulea Hockey & COOP Norrbotten Arena" />
+</head>
+<body class="page-index .menu-open">
+    <header>
+        <div class="header">
+            <div class="header-inner">
+                <a href="/" class="header-logo">
+                    <img src="/images/logo.png" alt="Cinstar"/>
+                </a>
+                <div class="header-menu mobile-menu" id="header-menu">
+                    <ul class="header-menu-ul">
+                        <li class="menu-item" id="in0">
+                            <a href="/" class="menu-link">HOME</a>
+                            <div class="subMenu">
+                                <ul class="subMenu-ul"></ul>
+                            </div>
+                        </li>
+                        <li class="menu-item" id="in01">
+                            <a href="/about" class="menu-link">ABOUT</a>
+                            <div class="subMenu">
+                                <ul class="subMenu-ul"></ul>
+                            </div>
+                        </li>
+                        <li class="menu-item" id="in02">
+                            <div class="menu-with-icon">
+                                <a href="/products" class="menu-link has-sub">PRODUCTS</a>
+                                <div class="menu-icon">
+                                    <i class="fa fa-chevron-right"></i>
+                                </div>
+                            </div>
+                            <div class="subMenu">
+                                <ul class="subMenu-ul">
+                                    <li class="subMenu-item">
+                                        <div class="menu-with-icon">
+                                            <a class="subMenu-link" href="/products/rental-led-display-screen.php">
+                                                <span>Rental LED Display Screen</span>
+                                            </a>
+                                            <div class="menu-icon">
+                                                <i class="fa fa-chevron-right"></i>
+                                            </div>
+                                        </div>
+                                        <div class="thirdMenu-item">
+                                            <a class="subMenu-link" href="/products/event-led-screen.php">
+                                                <span>LED Screens For Events</span>
+                                            </a>
+                                            <a class="subMenu-link" href="/products/church-led-screen.php">
+                                                <span>LED Screen For Church</span>
+                                            </a>
+                                            <a class="subMenu-link" href="/products/led-stage-screen.php">
+                                                <span>LED Stage Screen</span>
+                                            </a>
+                                            <a class="subMenu-link" href="/products/concert-led-screen.php">
+                                                <span>Concert LED Screen</span>
+                                            </a>
+                                        </div>
+                                    </li>
+                                    <li class="subMenu-item">
+                                        <div class="menu-with-icon">
+                                            <a class="subMenu-link" href="/products/outdoor-led-display-screen.php ">
+                                                <span>Outdoor LED Display Screen</span>
+                                            </a>
+                                            <div class="menu-icon">
+                                                <i class="fa fa-chevron-right"></i>
+                                            </div>
+                                        </div>
+                                        <div class="thirdMenu-item">
+                                            <a class="subMenu-link" href="/products/led-advertising.php">
+                                                <span>LED Advertising Display</span>
+                                            </a>
+                                            <a class="subMenu-link" href="/products/digital-led-billboard.php">
+                                                <span>Digital LED Billboard</span>
+                                            </a>
+                                            <a class="subMenu-link" href="/products/3d-led-display-screen.php">
+                                                <span>3D LED Display</span>
+                                            </a>
+                                            <a class="subMenu-link" href="/products/commercial-led-display.php">
+                                                <span>Commercial LED Display</span>
+                                            </a>
+                                        </div>
+                                    </li>
+                                    <li class="subMenu-item">
+                                        <a class="subMenu-link" href="/products/direct-view-led.php">
+                                            <span>Direct View LED</span>
+                                        </a>
+                                    </li>
+                                    <li class="subMenu-item">
+                                        <a class="subMenu-link" href="/products/fine-pitch-led-display.php">
+                                            <span>Fine Pitch LED Display</span>
+                                        </a>
+                                    </li>
+                                    <li class="subMenu-item">
+                                        <a class="subMenu-link" href="/products/indoor-led-display.php">
+                                            <span>Indoor LED Display</span>
+                                        </a>
+                                    </li>
+                                    <li class="subMenu-item">
+                                        <a class="subMenu-link" href="/products/custom-led-display.php">
+                                            <span>Custom LED Display</span>
+                                        </a>
+                                    </li>
+                                    <li class="subMenu-item">
+                                        <a class="subMenu-link" href="/products/creative-led-display.php">
+                                            <span>Creative LED Display</span>
+                                        </a>
+                                    </li>
+                                    <li class="subMenu-item">
+                                        <a class="subMenu-link" href="/products/stadium-led-screen.php">
+                                            <span>Stadium LED Screen</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="menu-item" id="in04">
+                            <div class="menu-with-icon">
+                                <a href="/gallery" class="menu-link has-sub">GALLERY</a>
+                                <div class="menu-icon">
+                                    <i class="fa fa-chevron-right"></i>
+                                </div>
+                            </div>
+                            <div class="subMenu">
+                                <ul class="subMenu-ul">
+                                    <li class="subMenu-item">
+                                        <a class="subMenu-link" href="/gallery-detail/rental-cases.php">
+                                            <span>Rental Event Cases</span>
+                                        </a>
+                                    </li>
+                                    <li class="subMenu-item">
+                                        <a class="subMenu-link" href="/gallery-detail/fixed-cases.php">
+                                            <span>Fixed Install Cases</span>
+                                        </a>
+                                    </li>
+                                    <li class="subMenu-item">
+                                        <a class="subMenu-link" href="/gallery-detail/fine-pitch-cases.php">
+                                            <span>Fine Pitch Cases</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="menu-item" id="in05">
+                            <a href="/led-academy" class="menu-link">LED ACADEMY</a>
+                            <div class="subMenu">
+                                <ul class="subMenu-ul"></ul>
+                            </div>
+                        </li>
+                        <li class="menu-item" id="in06">
+                            <a href="/news" class="menu-link">NEWS</a>
+                            <div class="subMenu">
+                                <ul class="subMenu-ul"></ul>
+                            </div>
+                        </li>
+                        <li class="menu-item" id="in07">
+                            <a href="/contact" class="menu-link">CONTACT</a>
+                            <div class="subMenu">
+                                <ul class="subMenu-ul"></ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <!-- mobile header开始 -->
+                <div class="mobile-header" id="mobile-header">
+                    <span class="iconfont menu">&#xe660;</span>
+                    <span class="fa fa-close"></span>
+                </div>
+                <!-- mobile header 结束 -->
+            </div>
+        </div>
+        <div class="whatsicon">
+            <a href="https://api.whatsapp.com/send?l=en&phone=8618320889297" target="_blank">
+                <i class="fa fa-whatsapp"></i>
+            </a>
+        </div>
+    </header>
+    <div class="error">
+        <div class="error-content">
+            <div class="error-tips">
+                <img src="/images/404-error.webp" alt="page not found" />
+                <p>Page Not Found</p>
+            </div>
+        </div>
+        <div class="error-product">
+            <h1>Products You Might Be Interested</h1>
+            <div class="product-container">
+                <div class="product-item">
+                    <a class="thumb" href="/product-detail/ch-series.php">
+                        <img src="/images/products/Fine-Pitch/CH-series-1.webp" alt="ch-series" class="short" style="width: 93%" />
+                    </a>
+                    <a class="list-title" href="/product-detail/ch-series.php">
+                        <span class="current">DVLED COB</span><span class="more">CH Series</span>
+                    </a>
+                </div>
+                <div class="product-item">
+                    <a class="thumb" href="/product-detail/ec-series.php">
+                        <img src="/images/products/Fine-Pitch/EC-series-1.webp" alt="ec-series" style="width: 93%"/>
+                    </a>
+                    <a class="list-title" href="product-detail/ec-series.php">
+                        <span class="current">Fine Pitch LED Display</span><span class="more">EC Series</span>
+                    </a>
+                </div>
+                <div class="product-item">
+                    <a class="thumb" href="product-detail/hd-series.php">
+                        <img src="/images/products/Fine-Pitch/HD-SERIES-2.webp" alt="hd-series"  style="width: 93%"/>
+                    </a>
+                    <a class="list-title" href="product-detail/hd-series.php">
+                        <span class="current">Direct View LED Video Wall</span><span class="more">HD Series</span>
+                    </a>
+                </div>
+                <div class="product-item">
+                    <a class="thumb" href="/product-detail/a-series.php">
+                        <img src="/images/products/Rental-LED/A-series-1.webp" alt="a-series" class="short" style="width: 95%" />
+                    </a>
+                    <a class="list-title" href="/product-detail/a-series.php">
+                        <span class="current">Stage LED Video Screen Wall</span><span class="more">A Series</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="contact-detail product-section">
+            <div class="head-title">
+                <h2>Get In Touch</h2>
+            </div>
+            <form id="myforms" name="myforms" method="post" action="/quotes/Index/index" onSubmit="return Checks()">
+                <div class="contact-body">
+                    <p class="input-contact">
+                        <input type="text" placeholder="Name" maxlength="50" name="name" class="contact-input">
+                        <input type="text" placeholder="Company" maxlength="50" name="company" class="contact-input">
+                    </p>
+                    <p class="input-contact">
+                        <input type="text" placeholder="E-mail" maxlength="50" name="email" class="contact-input">
+                        <input type="text" placeholder="Telephone" maxlength="50" name="tel" class="contact-input">
+                    </p>
+                    <textarea placeholder="Message" name="contents" maxlength="1000" class="contact-textarea"></textarea>
+                    <div class="btn">
+                        <button class="submit">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="copy">
+        Copyright 2012-2023 &copy; Cinstar Electronics. All Rights Reserved
+    </div>
+</body> 
+</html>
