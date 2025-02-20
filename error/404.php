8a544c8768188ad049db1027142c391cfc6f9fee
qidaiqiji <459306493@qq.com>
2025-02-20 18:57:37 +0800

    404页面修改链接

--- error/404.php
+++ error/404.php
@@ -6,36 +6,36 @@
     <meta name="viewport"
             content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="/css/common-240917.css">
-    <link rel="stylesheet" href="/css/contact-241024.css">
-    <link rel="stylesheet" href="/css/404.css">
+    <link rel="stylesheet" href="https://www.cinstar-led.com/css/reset.css">
+    <link rel="stylesheet" href="https://www.cinstar-led.com/css/common-240917.css">
+    <link rel="stylesheet" href="https://www.cinstar-led.com/css/contact-241024.css">
+    <link rel="stylesheet" href="https://www.cinstar-led.com/css/404.css">
     <title>404 Not Found | Cinstar LED</title>
 </head>
 <body class="page-index .menu-open">
     <header>
         <div class="header">
             <div class="header-inner">
-                <a href="/" class="header-logo">
-                    <img src="/images/logo.png" alt="Cinstar">
+                <a href="https://www.cinstar-led.com/" class="header-logo">
+                    <img src="https://www.cinstar-led.com/images/logo.png" alt="Cinstar">
                 </a>
                 <div class="header-menu mobile-menu" id="header-menu">
                     <ul class="header-menu-ul">
                         <li class="menu-item" id="in0">
-                            <a href="/" class="menu-link">HOME</a>
+                            <a href="https://www.cinstar-led.com/" class="menu-link">HOME</a>
                             <div class="subMenu">
                                 <ul class="subMenu-ul"></ul>
                             </div>
                         </li>
                         <li class="menu-item" id="in01">
-                            <a href="/about" class="menu-link">ABOUT</a>
+                            <a href="https://www.cinstar-led.com/about" class="menu-link">ABOUT</a>
                             <div class="subMenu">
                                 <ul class="subMenu-ul"></ul>
                             </div>
                         </li>
                         <li class="menu-item" id="in02">
                             <div class="menu-with-icon">
-                                <a href="/products" class="menu-link has-sub">PRODUCTS</a>
+                                <a href="https://www.cinstar-led.com/products" class="menu-link has-sub">PRODUCTS</a>
                                 <div class="menu-icon">
                                     <i class="fa fa-chevron-right"></i>
                                 </div>
@@ -44,7 +44,7 @@
                                 <ul class="subMenu-ul">
                                     <li class="subMenu-item">
                                         <div class="menu-with-icon">
-                                            <a class="subMenu-link" href="/products/rental-led-display-screen.php">
+                                            <a class="subMenu-link" href="https://www.cinstar-led.com/products/rental-led-display-screen.php">
                                                 <span>Rental LED Display Screen</span>
                                             </a>
                                             <div class="menu-icon">
@@ -52,23 +52,23 @@
                                             </div>
                                         </div>
                                         <div class="thirdMenu-item">
-                                            <a class="subMenu-link" href="/products/event-led-screen.php">
+                                            <a class="subMenu-link" href="https://www.cinstar-led.com/products/event-led-screen.php">
                                                 <span>LED Screens For Events</span>
                                             </a>
-                                            <a class="subMenu-link" href="/products/church-led-screen.php">
+                                            <a class="subMenu-link" href="https://www.cinstar-led.com/products/church-led-screen.php">
                                                 <span>LED Screen For Church</span>
                                             </a>
-                                            <a class="subMenu-link" href="/products/led-stage-screen.php">
+                                            <a class="subMenu-link" href="https://www.cinstar-led.com/products/led-stage-screen.php">
                                                 <span>LED Stage Screen</span>
                                             </a>
-                                            <a class="subMenu-link" href="/products/concert-led-screen.php">
+                                            <a class="subMenu-link" href="https://www.cinstar-led.com/products/concert-led-screen.php">
                                                 <span>Concert LED Screen</span>
                                             </a>
                                         </div>
                                     </li>
                                     <li class="subMenu-item">
                                         <div class="menu-with-icon">
-                                            <a class="subMenu-link" href="/products/outdoor-led-display-screen.php ">
+                                            <a class="subMenu-link" href="https://www.cinstar-led.com/products/outdoor-led-display-screen.php ">
                                                 <span>Outdoor LED Display Screen</span>
                                             </a>
                                             <div class="menu-icon">
@@ -76,52 +76,52 @@
                                             </div>
                                         </div>
                                         <div class="thirdMenu-item">
-                                            <a class="subMenu-link" href="/products/led-advertising.php">
+                                            <a class="subMenu-link" href="https://www.cinstar-led.com/products/led-advertising.php">
                                                 <span>LED Advertising Display</span>
                                             </a>
-                                            <a class="subMenu-link" href="/products/digital-led-billboard.php">
+                                            <a class="subMenu-link" href="https://www.cinstar-led.com/products/digital-led-billboard.php">
                                                 <span>Digital LED Billboard</span>
                                             </a>
-                                            <a class="subMenu-link" href="/products/3d-led-display-screen.php">
+                                            <a class="subMenu-link" href="https://www.cinstar-led.com/products/3d-led-display-screen.php">
                                                 <span>3D LED Display</span>
                                             </a>
-                                            <a class="subMenu-link" href="/products/commercial-led-display.php">
+                                            <a class="subMenu-link" href="https://www.cinstar-led.com/products/commercial-led-display.php">
                                                 <span>Commercial LED Display</span>
                                             </a>
                                         </div>
                                     </li>
                                     <li class="subMenu-item">
-                                        <a class="subMenu-link" href="/products/direct-view-led.php">
+                                        <a class="subMenu-link" href="https://www.cinstar-led.com/products/direct-view-led.php">
                                             <span>Direct View LED</span>
                                         </a>
                                     </li>
                                     <li class="subMenu-item">
-                                        <a class="subMenu-link" href="/products/fine-pitch-led-display.php">
+                                        <a class="subMenu-link" href="https://www.cinstar-led.com/products/fine-pitch-led-display.php">
                                             <span>Fine Pitch LED Display</span>
                                         </a>
                                     </li>
                                     <li class="subMenu-item">
-                                        <a class="subMenu-link" href="../products/transparent-led-film-screen.php">
+                                        <a class="subMenu-link" href="https://www.cinstar-led.com/products/transparent-led-film-screen.php">
                                             <span>Transparent LED Film Screen</span>
                                         </a>
                                     </li>
                                     <li class="subMenu-item">
-                                        <a class="subMenu-link" href="/products/indoor-led-display.php">
+                                        <a class="subMenu-link" href="https://www.cinstar-led.com/products/indoor-led-display.php">
                                             <span>Indoor LED Display</span>
                                         </a>
                                     </li>
                                     <li class="subMenu-item">
-                                        <a class="subMenu-link" href="/products/custom-led-display.php">
+                                        <a class="subMenu-link" href="https://www.cinstar-led.com/products/custom-led-display.php">
                                             <span>Custom LED Display</span>
                                         </a>
                                     </li>
                                     <li class="subMenu-item">
-                                        <a class="subMenu-link" href="/products/creative-led-display.php">
+                                        <a class="subMenu-link" href="https://www.cinstar-led.com/products/creative-led-display.php">
                                             <span>Creative LED Display</span>
                                         </a>
                                     </li>
                                     <li class="subMenu-item">
-                                        <a class="subMenu-link" href="/products/stadium-led-screen.php">
+                                        <a class="subMenu-link" href="https://www.cinstar-led.com/products/stadium-led-screen.php">
                                             <span>Stadium LED Screen</span>
                                         </a>
                                     </li>
@@ -130,7 +130,7 @@
                         </li>
                         <li class="menu-item" id="in04">
                             <div class="menu-with-icon">
-                                <a href="/gallery" class="menu-link has-sub">GALLERY</a>
+                                <a href="https://www.cinstar-led.com/gallery" class="menu-link has-sub">GALLERY</a>
                                 <div class="menu-icon">
                                     <i class="fa fa-chevron-right"></i>
                                 </div>
@@ -138,17 +138,17 @@
                             <div class="subMenu">
                                 <ul class="subMenu-ul">
                                     <li class="subMenu-item">
-                                        <a class="subMenu-link" href="/gallery-detail/rental-cases.php">
+                                        <a class="subMenu-link" href="https://www.cinstar-led.com/gallery-detail/rental-cases.php">
                                             <span>Rental Event Cases</span>
                                         </a>
                                     </li>
                                     <li class="subMenu-item">
-                                        <a class="subMenu-link" href="/gallery-detail/fixed-cases.php">
+                                        <a class="subMenu-link" href="https://www.cinstar-led.com/gallery-detail/fixed-cases.php">
                                             <span>Fixed Install Cases</span>
                                         </a>
                                     </li>
                                     <li class="subMenu-item">
-                                        <a class="subMenu-link" href="/gallery-detail/fine-pitch-cases.php">
+                                        <a class="subMenu-link" href="https://www.cinstar-led.com/gallery-detail/fine-pitch-cases.php">
                                             <span>Fine Pitch Cases</span>
                                         </a>
                                     </li>
@@ -156,19 +156,19 @@
                             </div>
                         </li>
                         <li class="menu-item" id="in05">
-                            <a href="/led-academy" class="menu-link">LED ACADEMY</a>
+                            <a href="https://www.cinstar-led.com/led-academy" class="menu-link">LED ACADEMY</a>
                             <div class="subMenu">
                                 <ul class="subMenu-ul"></ul>
                             </div>
                         </li>
                         <li class="menu-item" id="in06">
-                            <a href="/news" class="menu-link">NEWS</a>
+                            <a href="https://www.cinstar-led.com/news" class="menu-link">NEWS</a>
                             <div class="subMenu">
                                 <ul class="subMenu-ul"></ul>
                             </div>
                         </li>
                         <li class="menu-item" id="in07">
-                            <a href="/contact" class="menu-link">CONTACT</a>
+                            <a href="https://www.cinstar-led.com/contact" class="menu-link">CONTACT</a>
                             <div class="subMenu">
                                 <ul class="subMenu-ul"></ul>
                             </div>
@@ -192,7 +192,7 @@
     <div class="error">
         <div class="error-content">
             <div class="error-tips">
-                <img src="/images/404-error.webp" alt="page not found">
+                <img src="https://www.cinstar-led.com/images/404-error.webp" alt="page not found">
                 <p>Page Not Found</p>
             </div>
         </div>
@@ -200,34 +200,34 @@
             <h1>Products You Might Be Interested</h1>
             <div class="product-container">
                 <div class="product-item">
-                    <a class="thumb" href="/product-detail/ch-series.php">
-                        <img src="/images/products/Fine-Pitch/CH-series-1.webp" alt="ch-series" class="short" style="width: 93%">
+                    <a class="thumb" href="https://www.cinstar-led.com/product-detail/ch-series.php">
+                        <img src="https://www.cinstar-led.com/images/products/Fine-Pitch/CH-series-1.webp" alt="ch-series" class="short" style="width: 93%">
                     </a>
-                    <a class="list-title" href="/product-detail/ch-series.php">
+                    <a class="list-title" href="https://www.cinstar-led.com/product-detail/ch-series.php">
                         <span class="current">DVLED COB</span><span class="more">CH Series</span>
                     </a>
                 </div>
                 <div class="product-item">
-                    <a class="thumb" href="/product-detail/ec-series.php">
-                        <img src="/images/products/Fine-Pitch/EC-series-1.webp" alt="ec-series" style="width: 93%">
+                    <a class="thumb" href="https://www.cinstar-led.com/product-detail/ec-series.php">
+                        <img src="https://www.cinstar-led.com/images/products/Fine-Pitch/EC-series-1.webp" alt="ec-series" style="width: 93%">
                     </a>
-                    <a class="list-title" href="product-detail/ec-series.php">
+                    <a class="list-title" href="https://www.cinstar-led.com/product-detail/ec-series.php">
                         <span class="current">Fine Pitch LED Display</span><span class="more">EC Series</span>
                     </a>
                 </div>
                 <div class="product-item">
-                    <a class="thumb" href="product-detail/hd-series.php">
-                        <img src="/images/products/Fine-Pitch/HD-SERIES-2.webp" alt="hd-series"  style="width: 93%">
+                    <a class="thumb" href="https://www.cinstar-led.com/product-detail/hd-series.php">
+                        <img src="https://www.cinstar-led.com/images/products/Fine-Pitch/HD-SERIES-2.webp" alt="hd-series"  style="width: 93%">
                     </a>
-                    <a class="list-title" href="product-detail/hd-series.php">
+                    <a class="list-title" href="https://www.cinstar-led.com/product-detail/hd-series.php">
                         <span class="current">Direct View LED Video Wall</span><span class="more">HD Series</span>
                     </a>
                 </div>
                 <div class="product-item">
-                    <a class="thumb" href="/product-detail/a-series.php">
-                        <img src="/images/products/Rental-LED/A-series-1.webp" alt="a-series" class="short" style="width: 95%">
+                    <a class="thumb" href="https://www.cinstar-led.com/product-detail/a-series.php">
+                        <img src="https://www.cinstar-led.com/images/products/Rental-LED/A-series-1.webp" alt="a-series" class="short" style="width: 95%">
                     </a>
-                    <a class="list-title" href="/product-detail/a-series.php">
+                    <a class="list-title" href="https://www.cinstar-led.com/product-detail/a-series.php">
                         <span class="current">Stage LED Video Screen Wall</span><span class="more">A Series</span>
                     </a>
                 </div>
